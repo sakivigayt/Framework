ff93c442e0382225a93354252f5098dd4aabbd6e
cbhaka <cbhaka@DLUCBHAKA46325.sapient.com>
2016-12-01 12:45:37 +0530

    commit

--- src/main/java/com/utils/JsonReader.java
+++ src/main/java/com/utils/JsonReader.java
@@ -12,29 +12,45 @@ public class JsonReader {
 
 	@SuppressWarnings("unchecked")
 	public static String setCapability(String driverName, String capabilityName) {
-		final Map<String, Object> MAP = loadJsonToMap("Capabilities.json");
-
-		Map<String, String> BROWSER = (Map<String, String>) MAP.get(driverName);
-
+		System.out.println("Driver name is :: " + driverName);
+		System.out.println("Inside set capability");
+		final Map<String, Object> returnedMap = loadJsonToMap("Capabilities.json");
+		System.out.println("Map returned");
+		System.out.println(returnedMap.get("firefox"));
+		Map<String, String> BROWSER = (Map<String, String>) returnedMap.get(driverName);
+		System.out.println("Browser fetched on driver name");
 		String capability = BROWSER.get(capabilityName);
+		System.out.println(capability);
 		return capability;
 
 	}
 
 	@SuppressWarnings("unchecked")
 	public static Map<String, Object> loadJsonToMap(String jsonFile) {
+		System.out.println("Inside Load to json");
 		Map<String, Object> map = null;
 
 		GsonBuilder builder = new GsonBuilder();
 		builder.setPrettyPrinting().serializeNulls();
 		Gson gson = builder.create();
+		System.out.println("gson created");
 		try {
+			System.out.println("Inside try");
 			map = gson.fromJson(new FileReader(new File(jsonFile)), Map.class);
 		} catch (FileNotFoundException e) {
 
 		}
-
+		System.out.println("return map");
 		return map;
 	}
+	
+	public static String getCap() {
+		return setCapability("firefox", "browserName");
+	}
 
+	public static void main(String args[]) {
+		
+		System.out.println(getCap());
+		
+	}
 }
