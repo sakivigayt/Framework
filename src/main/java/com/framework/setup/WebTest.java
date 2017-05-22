c3d3d88616aac4650ba5bc48bdf63c6781ed5a9a
Shobhit Gupta <sgupta118@sapient.com>
2017-05-22 22:44:07 +0530

    Updated webtest

--- src/main/java/com/framework/setup/WebTest.java
+++ src/main/java/com/framework/setup/WebTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import com.applitools.eyes.Eyes;
+import com.drivers.DriverConfig;
 import com.drivers.DriverFactory;
 import com.relevantcodes.extentreports.ExtentTest;
 
@@ -21,17 +22,20 @@ public class WebTest {
 	 */
 	protected WebDriver driver;
 
-	protected Eyes eyes;
+	//protected Eyes eyes;
 	
 
 	@BeforeMethod
 	public void setup(ITestContext test) {
-
-		DriverFactory.initiateDriver(test.getCurrentXmlTest().getParameter(
-				"browser"));
-		driver = DriverFactory.getDriver();
-		eyes = new Eyes();
-		eyes.setApiKey("1nSenw9MMwrjI3eaqNkNUspVj8nmmdobaW4GaHA106UJU110");
+  System.out.println("RUNNING TEST");
+  DriverConfig config=new DriverConfig();
+	config.setBrowserName(test.getCurrentXmlTest().getParameter("browser"));
+	DriverFactory.initiateDriver();
+	DriverFactory.setDriverConfig(config);
+	/*	DriverFactory.initiateDriver();
+		driver = DriverFactory.getDriver()*/;
+	/*	eyes = new Eyes();
+		eyes.setApiKey("1nSenw9MMwrjI3eaqNkNUspVj8nmmdobaW4GaHA106UJU110");*/
 	}
 
 	@AfterMethod
